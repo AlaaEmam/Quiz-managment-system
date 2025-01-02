f3dbcbf27860ae9d2bcd92f865f240fdc5f14bb9
siegfried2020 <domydodo2015@gmail.com>
2025-01-02 20:08:45 +0200

    reset-password-2

--- src/modules/Auth/Forgot Password/components/ForgotPassword.tsx
+++ src/modules/Auth/Forgot Password/components/ForgotPassword.tsx
@@ -1,32 +1,48 @@
 import { Link, useNavigate } from "react-router-dom";
 import auth_pic from "../../../../assets/auth pic.png"
 import { useForm } from 'react-hook-form';
+import { Auth, axiosInstance } from "../../../Shared/Url/components/URL";
+import { EMAIL_VALIDATION } from "../../../Shared/Url/components/validations";
 
+
+
+interface formData{
+  email:string;
+}
 
 export default function ForgotPassword() {
   const navigate=useNavigate();
-  // let {register, formState:{isSubmitting, errors}, handleSubmit}=useForm();
+  let {register, formState:{isSubmitting, errors}, handleSubmit}=useForm<formData>();
+  
+  const onSubmit=async(data:formData)=>{
+    try{
+      const response= await axiosInstance.post(Auth.forgotPassword);
+      navigate('/auth/reset-password', {state:data.email});
+    }catch(error){
+      console.log(error)
+    }
+  }
+  
+  
   return (
   <div className="bg-black text-white">
     <div className="grid grid-cols-2">
       <div className="ml-40">
-        <span className="border-2 p-2 rounded-lg
-          border-solid 
-          border-white">
-            <i className=" bi bi-check-circle fs-5 "></i>
-        </span>
+        
         <h1 className="text-green-300 mt-40 mb-20 
          sm:text-sm
          md:text-base
          lg:text-2xl
          xl:text-4xl">Forget Password</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label className="">Email address</label>
           
+          {errors.email && <span className="text-danger ">{errors.email.message}</span>}
+                
           <div className="mt-3 border-2 
             rounded-lg
             border-solid 
-          border-white flex items-center rounded-md">
+          border-white flex items-center">
             <span className="border-2 p-2 border-none rounded-lg
     
             border-white">
@@ -35,16 +51,24 @@ export default function ForgotPassword() {
             <input placeholder="Type your Email here" 
             className="
             p-2
-            w-96  bg-black"/>
+            w-full  bg-black"
+            {...register('email',EMAIL_VALIDATION)
+            }
+            />
           </div>
 
           
 
-          <button className="mt-10 rounded-lg bg-white text-black p-2 " type="submit">Send email</button>
+          <button className="mt-10 rounded-lg bg-white
+           text-black p-2 " type="submit">{isSubmitting? "sending Email":'Send Email'} 
+           
+            <i className=" bi bi-check-circle-fill ml-2 text-xl "></i>
+        
+           </button>
         </form>
         <div className="flex justify-end xl:mt-10 lg:mt-5 md:mt-3 ">
           <p>
-            Login? <Link to={"auth/login"} className="text-yellow-200">click here</Link>  
+            Login? <Link to={"/auth/login"} className="text-yellow-200">click here</Link>  
           </p>
         </div>
       </div>
--- src/modules/Auth/Reset password/components/ResetPassword.tsx
+++ src/modules/Auth/Reset password/components/ResetPassword.tsx
@@ -1,9 +1,27 @@
 import { Link, useNavigate } from "react-router-dom";
 import auth_pic from "../../../../assets/auth pic.png"
 import { useForm } from 'react-hook-form';
+import { EMAIL_VALIDATION, PasswordValidation } from "../../../Shared/Url/components/validations";
+import { Auth, axiosInstance } from "../../../Shared/Url/components/URL";
 
-
+interface formData{
+  email:string;
+  otp:string;
+  password:string;
+}
 export default function ResetPassword() {
+  const navigate=useNavigate();
+  const{register, handleSubmit, formState:{isSubmitting, errors}}=useForm<formData>()
+  
+  const onSubmit=async()=>{
+    try{
+      const response=await axiosInstance.post(Auth.resetPassword);
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
   return (
     <div className="bg-black text-white">
     <div className="grid grid-cols-2">
@@ -13,41 +31,89 @@ export default function ResetPassword() {
          sm:text-sm
          md:text-base
          lg:text-2xl
-         xl:text-4xl">Forget Password</h1>
-        <form>
-          <label className="">Email address</label>
+         xl:text-4xl">Reset Password</h1>
+        <form >
+          <label className="mt-5">Your Email address</label>
           
+          {errors.email && <span className="text-danger ">{errors.email.message}</span>}
+              
           <div className="mt-3 border-2 
             rounded-lg
             border-solid 
           border-white flex items-center rounded-md">
             <span className="border-2 p-2 border-none rounded-lg
-    
             border-white">
               <i className="bi bi-envelope text-xl"></i>
             </span>
             <input placeholder="Type your Email here" 
             className="
             p-2
-            w-96  bg-black"/>
+            w-96  bg-black"
+            {...register("email",EMAIL_VALIDATION)}/>
           </div>
 
-          
 
-          <button className="mt-10 rounded-lg 
-            bg-white text-black p-2 " 
+          <label className="mt-5">OTP</label>
+          
+          <div className="mt-3 border-2 
+            rounded-lg
+            border-solid 
+          border-white flex items-center rounded-md">
+            <span className="border-2 p-2 border-none rounded-lg
+            border-white">
+              <i className="bi bi-envelope text-xl"></i>
+            </span>
+            <input type="text" placeholder="Choose your otp" 
+            className="
+            p-2
+            w-96  bg-black"
+            {...register("otp")}/>
+          </div>
+
+          <label className="mt-5">Password</label>
+          
+          <div className="mt-3 border-2 
+            rounded-lg
+            border-solid 
+          border-white flex items-center rounded-md">
+            <span className="border-2 p-2 border-none rounded-lg
+            border-white">
+              <i className="bi bi-envelope text-xl"></i>
+            </span>
+            <input type="password" placeholder="Type your Password" 
+            className="
+            p-2
+            w-96  bg-black"
+            {...register("password",PasswordValidation("invalid Password"))}/>
+          </div>
+
+          <label className="mt-5">Confirm Password</label>
+          
+          <div className="mt-3 border-2 
+            rounded-lg
+            border-solid 
+          border-white flex items-center rounded-md">
+            <span className="border-2 p-2 border-none rounded-lg
+            border-white">
+              <i className="bi bi-envelope text-xl"></i>
+            </span>
+            <input type="password" placeholder="Type your confirm Password" 
+            className="
+            p-2
+            w-96  bg-black"
+            {...register("password",PasswordValidation("invalid Password"))}/>
+          </div>
+
+          <button className="mt-5 rounded-lg 
+            bg-white text-black p-2 px-4  text-center" 
             type="submit">
-              Send email
+              Reset
               <span className="">
-                <i className=" bi bi-check-circle-fill text-lg "></i>
+                <i className=" bi bi-check-circle-fill text-lg ml-2"></i>
               </span>
           </button>
         </form>
-        <div className="flex justify-end xl:mt-10 lg:mt-5 md:mt-3 ">
-          <p>
-            Login? <Link to={"/auth/login"} className="text-yellow-200">click here</Link>  
-          </p>
-        </div>
+        
       </div>
       <div className="p-14">
         <div className="bg-amber-200 rounded-2xl">
