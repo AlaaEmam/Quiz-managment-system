31e1a29cf2a7c4c5e95464f3d5489f84bc4e5e28
ziadyasserr <zyasser283@gmail.com>
2025-01-02 19:47:06 +0200

    forgetPass-Ui-Fixed

--- src/modules/Auth/Forgot Password/components/ForgotPassword.tsx
+++ src/modules/Auth/Forgot Password/components/ForgotPassword.tsx
@@ -1,75 +1,45 @@
-import { Link, useNavigate } from "react-router-dom";
-import auth_pic from "../../../../assets/auth pic.png"
-import { useForm } from 'react-hook-form';
-
-
-
-interface formData{
-  email:string;
-}
+import { Link } from 'react-router-dom';
 
 export default function ForgotPassword() {
-  const navigate=useNavigate();
-  let {register, formState:{isSubmitting, errors}, handleSubmit}=useForm<formData>();
-  
-  const onSubmit=async(data:formData)=>{
-    // const response= await;
-  }
-  
-  
   return (
-  <div className="bg-black text-white">
-    <div className="grid grid-cols-2">
-      <div className="ml-40">
-        
-        <h1 className="text-green-300 mt-40 mb-20 
-         sm:text-sm
-         md:text-base
-         lg:text-2xl
-         xl:text-4xl">Forget Password</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label className="">Email address</label>
-          
-          <div className="mt-3 border-2 
-            rounded-lg
-            border-solid 
-          border-white flex items-center rounded-md">
-            <span className="border-2 p-2 border-none rounded-lg
-    
-            border-white">
-              <i className="bi bi-envelope text-xl"></i>
-            </span>
-            <input placeholder="Type your Email here" 
-            className="
-            p-2
-            w-96  bg-black"
-            {...register('email')
-            }
-            />
-          </div>
+    <>
+      <div className="">
+        <div>
+          <h2 className="text-green text-3xl mb-16">Forget Password</h2>
+        </div>
+        <div className='text-white'>
+          <form>
+            <label className="">Email address</label>
 
-          
+            <div className="mt-3   px-2 py-3  flex items-center justify-center border-[3px] border-white rounded-xl mb-20">
+              <span className=" px-3">
+                <i className="bi bi-envelope text-xl"></i>
+              </span>
+              <input
+                placeholder="Type your email "
+                className="  bg-inherit w-full   focus:outline-none "
+              />
+            </div>
 
-          <button className="mt-10 rounded-lg bg-white
-           text-black p-2 " type="submit">Send email 
-           
-            <i className=" bi bi-check-circle-fill ml-2 text-xl "></i>
-        
-           </button>
-        </form>
-        <div className="flex justify-end xl:mt-10 lg:mt-5 md:mt-3 ">
-          <p>
-            Login? <Link to={"/auth/login"} className="text-yellow-200">click here</Link>  
-          </p>
+            <button
+              className="mt-10 rounded-lg bg-white text-black px-3 py-2 flex items-center font-bold "
+              type="submit"
+            >
+              Send email
+              <i className=" bi bi-check-circle-fill ml-2 text-xl "></i>
+            </button>
+          </form>
+        </div>
+
+        <div className="flex justify-end mt-20 text-white gap-1 ">
+            Login?
+          <button className=''>
+            <Link to={'/login'} className="text-green underline">
+              click here
+            </Link>
+          </button>
         </div>
       </div>
-      <div className="p-14">
-        <div className="bg-amber-200 rounded-2xl">
-          <img src={auth_pic} alt="auth pic" />
-
-        </div>
-      </div>
-      
-    </div>
-  </div>);
+    </>
+  );
 }
