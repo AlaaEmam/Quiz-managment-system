f79b2ce7ca6b193936d6f2d6585f6ea2b8f34943
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2025-01-08 17:03:52 +0200

    finish top 5 student

--- src/modules/Admin/Dashboard/components/Dashboard.tsx
+++ src/modules/Admin/Dashboard/components/Dashboard.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UpcomingQuizzes from './UpcomingQuizzes';
 import TopStudents from './TopStudents';
+import { axiosInstance } from '../../../../Constants/URLS/URL';
+import { Student } from '../../../Shared/Url/components/URL';
 
-const Dashboard = ({ 
-  // incomingQuiz, topStudents 
-}) => {
+
+
+
+
+export default  function Dashboard () {
+const [topFiveStudents , setTopFiveStudents] = useState([]);
+
+  const getTopFiveStudents = async () => {
+    try {
+      const response = await axiosInstance.get(Student.gettopFiveStudents);
+      console.log(response.data);
+      setTopFiveStudents(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTopFiveStudents();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div className="col-span-1">
@@ -14,11 +34,10 @@ const Dashboard = ({
       </div>
       <div className="col-span-1">
         <TopStudents 
-        // students={topStudents}
+        students={topFiveStudents}
          />
       </div>
     </div>
   );
 };
 
-export default Dashboard;
--- src/modules/Admin/Dashboard/components/TopStudents.tsx
+++ src/modules/Admin/Dashboard/components/TopStudents.tsx
@@ -1,59 +1,89 @@
 import React from 'react';
-import image from '../../../../assets/user.png';
 import { Link } from 'react-router-dom';
 import { TiArrowRight } from 'react-icons/ti';
-const TopStudents = ({ 
-    // students 
-}) => {
+import { GoDotFill } from "react-icons/go";
+
+import image1 from '../../../../assets/user.svg';
+import image2 from '../../../../assets/user2.svg';
+import image3 from '../../../../assets/user3.svg';
+import image4 from '../../../../assets/user4.svg';
+import image5 from '../../../../assets/user5.svg';
+
+interface TopFiveStudents {
+  _id: number;
+  first_name: string;
+  last_name: string;
+  status: string;
+  avg_score: number;
+}
+
+interface TopStudentsProps {
+  students: TopFiveStudents[];
+}
+
+const TopStudents: React.FC<TopStudentsProps> = ({ students }) => {
+  const defaultAvatars = [image1, image2, image3, image4, image5];
+
   return (
     <div className="border-2 rounded-lg py-4 ps-4">
-   <div className="flex items-center justify-between  m-2 ">
-      <h3 className="font-bold text-xl tracking-wide mb-5">
-        Top 5 Students
-      </h3>
-      <div >
-        <Link to="student" className=" items-center ">
-          <div className="flex">
-          All Students <TiArrowRight className="text-3xl text-green" />
-          </div>
-        </Link>
+      <div className="flex items-center justify-between m-2">
+        <h3 className="font-bold text-xl tracking-wide mb-5">Top 5 Students</h3>
+        <div>
+          <Link to="student" className="items-center">
+            <div className="flex">
+              All Students <TiArrowRight className="text-3xl text-green" />
+            </div>
+          </Link>
+        </div>
       </div>
-   </div>
-      {/* {students.map((student) => ( */}
-        <div 
-        // key={student.id} 
-        className="flex items-center justify-between gap-x-4 border-2 rounded-lg m-2 overflow-hidden">
-          <div className='flex'>
-          <div className='user-image'>
-            <img 
-            src={image}
-            // src={student.profilePicture} 
-            // alt={student.name} 
-           />
-          </div>
-          <div className='p-4'>
-            <span className="block font-bold text-lg">
-            Emmanuel James
-                {/* {student.name} */}
-                </span>
-            <span className="block">
-            Class rank: 1th
-                {/* {student.rank} */}
+      {students.map((student, index) => (
+        <div key={student._id} className="flex items-center justify-between gap-x-4 border-2 rounded-lg m-2 overflow-hidden">
+          <div className="flex">
+            <div className="p-2">
+              {/* Use the default avatar based on index */}
+              <img 
+                src={defaultAvatars[index % defaultAvatars.length]} 
+                alt={`${student.first_name} ${student.last_name}`} 
+              />
+            </div>
+            <div className="p-4">
+              <span className="block font-bold text-lg">
+                {student.first_name} {student.last_name}
+              </span>
+              <span className="flex">
+                  {/* Status Button */}
+                  {student.status === 'active' ? (
+                        <span className="font-bold flex items-center text-lime-600  px-3 py-1">
+                          <span className="mr-1" >
+                          <GoDotFill className='text-lime-600'/>
+                          </span>
+                          Online
+                        </span>
+                      ) : (
+                        <span className="font-bold flex items-center text-red-700  px-3 py-1">
+                        <span className="mr-1" >
+                        <GoDotFill className='text-red-700'/>
+                        </span>
+                        Offline
+                      </span>
+                      )}               
                 <span className="mx-3">|</span>
-                Average score: 87%
-                {/* {student.score} */}
-            </span>
+                <span className="font-bold flex items-center text-yellow-400  px-3 py-1">
+                Avg Score: 
+                </span>
+                {student.avg_score.toFixed(2)}%
+              </span>
+            </div>
           </div>
-          </div>
-          <div className=''>
-            <Link to="student-details" className="flex items-center ">
+          <div>
+            <Link to="student-details" className="flex items-center">
               <div className="bg-slate-800 rounded-full p-1 text-white m-3">
                 <TiArrowRight className="text-3xl" />
               </div>
             </Link>
           </div>
-      {/* ))} */}
-    </div>
+        </div>
+      ))}
     </div>
   );
 };
