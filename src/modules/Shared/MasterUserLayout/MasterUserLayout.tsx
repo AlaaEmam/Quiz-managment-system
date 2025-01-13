7c58ba87e16a1b45f289615513f2563ba4fba6c8
alaa emam <66056822+AlaaEmam@users.noreply.github.com>
2025-01-13 11:47:35 +0200

    add sidebar student

--- src/modules/Shared/MasterUserLayout/MasterUserLayout.tsx
+++ src/modules/Shared/MasterUserLayout/MasterUserLayout.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../Navbar/Navbar';
+import StudentSideBar from '../StudentSideBar/StudentSideBar';
 
 export default function MasterUserLayout() {
+  
+    const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false);
+  
+    const toggleSidebar = () => {
+      setIsSidebarOpen(!isSidebarOpen);
+    };
   return (
  <>
-    <Outlet/>
+       <div>
+        <Navbar toggleSidebar={toggleSidebar} />
+
+        <div className="flex">
+          <StudentSideBar isStudentSideBarOpen={isSidebarOpen} />
+          <div className="w-full px-5 pt-5">
+            <Outlet />
+          </div>
+        </div>
+      </div>
     
  </>
   )
